725e671258b36d25079845972402485515cb4a0b
sziller <szillerke@gmail.com>
2023-10-05 23:33:01 +0200

    init args

--- SenseHatSensors/Class_SenseHatSensors.py
+++ SenseHatSensors/Class_SenseHatSensors.py
@@ -28,9 +28,15 @@ class EnvironmentalReadings:
     In order to work as intended, user needs a RaspberryPi with a SenseHat mounted.
     Each instance measures the set of data according to users choice, and scrolls these on the LED Display.
     ============================================================================================== by Sziller ==="""
-    def __init__(self, low_light: bool = True, **kwargs):
+    def __init__(self,
+                 code: int = 7,
+                 scroll_speed: int = 1,
+                 low_light: bool = True,
+                 **kwargs):
         self.sense                  = SenseHat()
         self.sense.low_light        = low_light
+        self.code: int              = code
+        self.scroll_speed: float    = scroll_speed
 
         # creating dictionary for the entire life-cycle of the object. All measurements will be stored here.
         self.sensehat_measurements_list: list = []  #
@@ -38,7 +44,7 @@ class EnvironmentalReadings:
         self.delta_t_h = 0
         self.delta_t_m = 0
         
-    def show_data(self, scroll_speed: float = 2.0, pointer: int = -1):
+    def show_data(self, pointer: int = -1):
         """=== Method name: show_data ==================================================================================
         Method shows 'pointer'-represented data read out of <self.sensehat_measurements_list> over the
         SenseHat-LED-Display.
@@ -49,20 +55,20 @@ class EnvironmentalReadings:
         if 'humidity' in self.sensehat_measurements_list[pointer]\
                 and self.sensehat_measurements_list[pointer]['humidity'] is not None:
             self.sense.show_message('rH:' + str(self.sensehat_measurements_list[pointer]['humidity']) + "%",
-                                    scroll_speed=scroll_speed,
+                                    scroll_speed=self.scroll_speed,
                                     text_colour=[0, 0, 255])
         if 'temperature' in self.sensehat_measurements_list[pointer]\
                 and self.sensehat_measurements_list[pointer]['temperature'] is not None:
             self.sense.show_message('T:' + str(self.sensehat_measurements_list[pointer]['temperature']) + "'C",
-                                    scroll_speed=scroll_speed,
+                                    scroll_speed=self.scroll_speed,
                                     text_colour=[255, 0, 0])
         if 'air_pressure' in self. sensehat_measurements_list[pointer]\
                 and self.sensehat_measurements_list[pointer]['air_pressure'] is not None:
             self.sense.show_message('p:' + str(self.sensehat_measurements_list[pointer]['air_pressure']) + 'bar',
-                                    scroll_speed=scroll_speed,
+                                    scroll_speed=self.scroll_speed,
                                     text_colour=[255, 255, 0])
     
-    def show_actual_data(self, code: int = 7, scroll_speed: float = 2):
+    def show_actual_data(self):
         """=== Method name: show_actual_data ===========================================================================
         Displaying actual environmental data. Picking data type is a simple addition:
         'measurement type'  1:  temperature
@@ -72,14 +78,13 @@ class EnvironmentalReadings:
         e.g.: temperature + humidity = 3
         Method displays data stored in Instance RIGHT AFTER actualizing said data.
         ========================================================================================== by Sziller ==="""
-        self.measure_sensehat_env_data(code=code)
-        self.show_data(scroll_speed=scroll_speed, pointer=-1)
+        self.measure_sensehat_env_data()
+        self.show_data(pointer=-1)
 
     def write_data_to_disc(self, db_session):
-        
         pass
 
-    def measure_sensehat_env_data(self, code):
+    def measure_sensehat_env_data(self):
         """=== Method name: measure_sensehat_env_data ==================================================================
         Method measures and stores environmental readings
         'code' behaviour:   1:  temperature
@@ -87,10 +92,8 @@ class EnvironmentalReadings:
                             4:  pressure
         You simply add the integers of each measurement type, you want to read:
         e.g.: temperature + humidity = 3
-        :param code:
         :return:
         ========================================================================================== by Sziller ==="""
-
         current_system_time = time.time() - 3600 * self.delta_t_h - 60 * self.delta_t_m
         dst = time.gmtime(current_system_time)  # Detailed System Time
         curr_date_str = time.strftime("%Y-%m-%d", dst)
@@ -102,14 +105,14 @@ class EnvironmentalReadings:
                            'air_pressure': None,
                            'warning': None}
 
-        if code in [1, 3, 5, 7]:
+        if self.code in [1, 3, 5, 7]:
             temp = round(self.sense.get_temperature_from_pressure(), 1)
             cpu_temp = round(float(measure_cpu_temp()), 1)
             factor = 1
-            current_dataset['temperature']  = round((temp - cpu_temp)/factor, 1)
-        if code in [2, 3, 6, 7]:
+            current_dataset['temperature']  = round((temp - cpu_temp) * factor, 1)
+        if self.code in [2, 3, 6, 7]:
             current_dataset['humidity']     = round(self.sense.get_humidity(), 1)
-        if code in [4, 5, 6, 7]:
+        if self.code in [4, 5, 6, 7]:
             current_dataset['air_pressure'] = round(self.sense.get_pressure(), 1)
         
         self.sensehat_measurements_list.append(dict(current_dataset))
@@ -126,4 +129,4 @@ def measure_cpu_temp():
 
 if __name__ == "__main__":
     sensor = EnvironmentalReadings()
-    sensor.show_actual_data(code=7, scroll_speed=0.1)
+    sensor.show_actual_data()
